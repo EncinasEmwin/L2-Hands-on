662cd8caaf75742891ab3f659da8e4889b63ffd5
EncinasEmwin <encinasemwin17@gmail.com>
2025-02-23 22:22:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function addNumbers(a, b) {
+  return a + b;
+}
+
+let num1 = parseFloat(prompt("Enter the first number:"));
+let num2 = parseFloat(prompt("Enter the second number:"));
+
+let sum = addNumbers(num1, num2);
+alert("The sum is: " + sum);
+
+console.log(addNumbers(5, 10)); // Output: 15
